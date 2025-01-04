718ee0d5cff24731fa72e9ab0a7cf1279cd20910
murilinhops <murilinhorps@gmail.com>
2025-01-04 13:19:45 -0300

    Adding transcriptor to test rolls

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Unlicense"
             :url "https://unlicense.org/"}
+  :profiles {:dev {:source-paths ["src" "repls"]}}
+  :aliases  {"repls-test" ["run" "-m" "repl-runner"]}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [com.cognitect/transcriptor "0.1.5"]
                  [org.clojure/spec.alpha "0.5.238"]]
--- src/clj_dice_roller/core.clj
+++ src/clj_dice_roller/core.clj
@@ -18,12 +18,12 @@
         lowest (apply min roll)]
        [roll lowest]))
 
+(def roll-multiple [])
+
 (roll 0 6) ;; () or []
 (roll 1 6) ;; [3], 1 a 6a
 (roll 3 6) ;; [1 4 6]
 (roll 2 6 {:modifier 2})
-(roll-keep-highest 3 10)
-(roll-keep-lowest 3 6)
 (roll 1 0) ;; ()
 (roll -1 6) ;; ()
 (roll 3 -1) ;; ()
