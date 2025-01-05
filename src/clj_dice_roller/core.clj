99bbaa405ee514b61924b90d121339d6f34ebbf7
murilinhops <murilinhorps@gmail.com>
2025-01-05 14:17:45 -0300

    Unify roll-multiple + sum-multiple-rolls

--- src/clj_dice_roller/core.clj
+++ src/clj_dice_roller/core.clj
@@ -1,6 +1,10 @@
 (ns clj-dice-roller.core
   (:require [clojure.string :as str]))
 
+;;TODO: add modifier to multiple rolls
+;;TODO: format roll-multiple output like rollem
+;;TODO: logic to choose between fn, if it should call roll, roll-multiple, or advantage and disavantage roll
+
 (defn parse-roll
   [roll]
   (->> (str/split roll #"d")
@@ -11,34 +15,35 @@
   (mapv #(if (coll? %) (remove-strings %)  %)
         (remove string? rolls)))
 
-(defn get-roll-value "Returns only the result from the roll form this ([15] 1d20)" 
-  [roll] 
+(defn ^:private sum-multiple-rolls
+  [rolls]
+  (let [remove-strs  (remove-strings rolls)
+        rolls-results (reduce #(apply conj % %2) remove-strs)
+        print-rolls (if (-> rolls count (= 1)) (first rolls) rolls)]
+    [(reduce + rolls-results) print-rolls]))
+
+(defn get-roll-value "Returns only the result from the roll form this ([15] 1d20)"
+  [roll]
   (first roll))
 
 (defn roll
   "Rolls some dice, like (roll 3 6) would be three d6."
   [amount dice & {:keys [modifier]}]
-  (let [result (->> (mapv #(inc (rand-int %)) (filter pos-int? (repeat amount dice))) 
-                  (mapv #(+ % (or modifier 0))))
+  (let [result (->> (mapv #(inc (rand-int %)) (filter pos-int? (repeat amount dice)))
+                    (mapv #(+ % (or modifier 0))))
         print-dice (str amount "d" dice)
         print-mod (when modifier (str "+" modifier))]
     (remove nil? [result print-dice print-mod])))
 
 (defn roll-multiple
   [& args]
-  (->> (reduce (fn [acc dice]
-                 (let [parsed-dice (parse-roll dice)
-                       roll (apply roll parsed-dice)]
-                   (conj acc [(get-roll-value roll) dice])))
-               [] args)
-       (mapv #(into (first %) [(second %)]))))
-
-(defn sum-multiple-rolls
-  [rolls]
-  (let [remove-strs  (remove-strings rolls)
-        rolls-results (reduce #(apply conj % %2) remove-strs)
-        print-rolls (if (-> rolls count (= 1)) (first rolls) rolls)]
-    [(reduce + rolls-results) print-rolls]))
+  (-> (->> (reduce (fn [acc dice]
+                     (let [parsed-dice (parse-roll dice)
+                           roll (apply roll parsed-dice)]
+                       (conj acc [(get-roll-value roll) dice])))
+                   [] args)
+           (mapv #(into (first %) [(second %)])))
+      (sum-multiple-rolls)))
 
 (defn roll-keep-highest
   [amount dice & {:keys [modifier] :or {modifier 0}}]
@@ -63,5 +68,5 @@
 (roll 3 -1) ;; ()
 (roll -3 -6) ;; ()
 (roll 1 20)
-(-> (roll-multiple "3d4") (sum-multiple-rolls)) ;; [6 [1 1 4 "3d4"]]
-(-> (roll-multiple "3d4" "1d6") (sum-multiple-rolls)) ;; [10 [[3 3 2 "3d4"] [2 "1d6"]]]
+(roll-multiple "3d4") ;; [6 [1 1 4 "3d4"]]
+(roll-multiple "3d4" "1d6") ;; [10 [[3 3 2 "3d4"] [2 "1d6"]]]
